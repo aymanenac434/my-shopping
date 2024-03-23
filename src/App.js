f6ed4c8db954d91b9e1599d500a8da28842ead76
aymanenac434 <146624352+aymanenac434@users.noreply.github.com>
2024-03-23 23:57:18 +0000

    jsx checkpoint
    jsx gmc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Card } from "react-bootstrap";
+import Name from "./Name";
+import Price from "./Price";
+import Description from "./Description";
+import Image from "./Image";
+import product from "./product";
 
-function App() {
+const App = () => {
+  const firstName = "Serab"; // Replace with your first name
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card style={{ width: "18rem", margin: "auto", marginTop: "20px" }}>
+        <Card.Body>
+          <Image image={product.image} />
+          <Name name={product.name} />
+          <Price price={product.price} />
+          <Description description={product.description} />
+        </Card.Body>
+      </Card>
+      <p style={{ textAlign: "center", marginTop: "20px" }}>
+        {firstName ? `Hello, ${firstName}!` : "Hello, there!"}
+      </p>
+      {firstName && <Image image={product.image} />}
     </div>
   );
-}
+};
 
 export default App;
